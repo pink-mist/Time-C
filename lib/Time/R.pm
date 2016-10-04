fda570adbf7547f81d68267925c3860c96f6e466
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2016-10-04 14:31:48 +0200

    Time::R: add some content to the doc stubs

--- lib/Time/R.pm
+++ lib/Time/R.pm
@@ -128,41 +128,114 @@ method new ($c: $start, :$end = undef, :$years = 0, :$months = 0, :$weeks = 0, :
 
 =head2 start
 
+  my $start = $r->start;
+  $r->start = $start;
+
+Returns or sets the L<Time::C> object representing the starting time of the recurrence. Setting this also calls C<< $r->reset() >>.
+
 =cut
 
 =head2 current
+
+  my $current = $r->current;
+  $r->current = $current;
+
+Returns or sets the L<Time::C> object representing the current time of the current recurrence.
+
+This may get changed by C<< $r->next >>, C<< $r->upcoming >>, C<< $r->latest >>, C<< $r->until >>, and C<< $r->reset >>.
 
 =cut
 
 =head2 end
 
+  my $end = $r->end;
+  $r->end = $end;
+
+Returns or sets the L<Time::C> object representing the end time of the recurrence.
+
 =cut
 
 =head2 years
+
+  my $years = $r->years;
+  $r->years = $years;
+  $r->years += 10;
+  $r->years++;
+  $r->years--;
+
+Returns or sets the number of years between each recurrence. Setting this also calls C<< $r->reset() >>.
 
 =cut
 
 =head2 months
 
+  my $months = $r->months;
+  $r->months = $months;
+  $r->months += 12;
+  $r->months++;
+  $r->months--;
+
+Returns or sets the number of months between each recurrence. Setting this also calls C<< $r->reset() >>.
+
 =cut
 
 =head2 weeks
+
+  my $weeks = $r->weeks;
+  $r->weeks = $weeks;
+  $r->weeks += 52;
+  $r->weeks++;
+  $r->weeks--;
+
+Returns or sets the number of weeks between each recurrence. Setting this also calls C<< $r->reset() >>.
 
 =cut
 
 =head2 days
 
+  my $days = $r->days;
+  $r->days = $days;
+  $r->days += 7;
+  $r->days++;
+  $r->days--;
+
+Returns or sets the number of days between each recurrence. Setting this also calls C<< $r->reset() >>.
+
 =cut
 
 =head2 hours
+
+  my $hours = $r->hours;
+  $r->hours = $hours;
+  $r->hours += 24;
+  $r->hours++;
+  $r->hours--;
+
+Returns or sets the number of hours between each recurrence. Setting this also calls C<< $r->reset() >>.
 
 =cut
 
 =head2 minutes
 
+  my $minutes = $r->minutes;
+  $r->minutes = $minutes;
+  $r->minutes += 60;
+  $r->minutes++;
+  $r->minutes--;
+
+Returns or sets the number of minutes between each recurrence. Setting this also calls C<< $r->reset() >>.
+
 =cut
 
 =head2 seconds
+
+  my $seconds = $r->seconds;
+  $r->seconds = $seconds;
+  $r->seconds += 60;
+  $r->seconds++;
+  $r->seconds--;
+
+Returns or sets the number of seconds between each recurrence. Setting this also calls C<< $r->reset() >>.
 
 =cut
 
@@ -172,21 +245,41 @@ method new ($c: $start, :$end = undef, :$years = 0, :$months = 0, :$weeks = 0, :
 
 =head2 next
 
+  my $next = $r->next();
+
+Sets C<< $r->current >> to and returns the next recurrence as a L<Time::C> object. If the next time would happen after the C<< $r->end >>, it instead returns C<undef> and leaves C<< $r->current >> alone.
+
 =cut
 
 =head2 upcoming
+
+  my $upcoming = $r->upcoming();
+
+Sets C<< $r->current >> to and returns the next time the recurrence occurs as a L<Time::C> object based on the current time. If the next time would happen after the C<< $r->end >>, it instead returns C<undef> and leaves C<< $r->current >> alone.
 
 =cut
 
 =head2 latest
 
+  my $latest = $r->latest();
+
+Sets C<< $r->current >> to and returns the latest time the recurrence occurs as a L<Time::C> object based on the current time.
+
 =cut
 
 =head2 until
 
+  my @until = $r->until($end);
+
+Returns all the recurrences that will happen from C<< $r->current >> until C<$end> (which should be a L<Time::C> object), and updates C<< $r->current >> to the last one returned if any. If C<$end> is after C<< $r->end >> if defined, it will instead use C<< $r->end >> as the limit.
+
 =cut
 
 =head2 reset
+
+  $r->reset();
+
+Resets C<< $r->current >> to C<< $r->start >>.
 
 =cut
 
