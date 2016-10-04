ab69424002de075d54cbe64ab9ad85bed4721767
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2016-10-04 13:53:48 +0200

    Time::R: add ->new constructor

--- lib/Time/R.pm
+++ lib/Time/R.pm
@@ -53,4 +53,73 @@ Convenient ways of handling recurrences.
 
 =cut
 
+=head2 new
+
+  my $r = Time::R->new($start);
+  my $r = Time::R->new($start, end => $end, years => $year, months => $months,
+    weeks => $weeks, days => $days, hours => $hours, minutes => $minutes,
+    seconds => $seconds);
+
+Creates a Time::R object starting at C<$start>, and optionally ending at C<$end>. Every argument except C<$start> is optional and can be in any order.
+
+=over
+
+=item C<$start>
+
+This should be a L<Time::C> object representing the starting time.
+
+=item C<$end>
+
+This should be a L<Time::C> object optionally specifying the ending time. Defaults to C<undef>.
+
+=item C<$years>
+
+This should be the number of years between each recurrence. Defaults to C<0>.
+
+=item C<$months>
+
+This should be the number of months between each recurrence. Defaults to C<0>.
+
+=item C<$weeks>
+
+This should be the number of weeks between each recurrence. Defaults to C<0>.
+
+=item C<$days>
+
+This should be the number of days between each recurrence. Defaults to C<0>.
+
+=item C<$hours>
+
+This should be the number of hours between each recurrence. Defaults to C<0>.
+
+=item C<$minutes>
+
+This should be the number of minutes between each recurrence. Defaults to C<0>.
+
+=item C<$seconds>
+
+This should be the number of seconds between each recurrence. Defaults to C<0>.
+
+=back
+
+=cut
+
+method new ($c: $start, :$end = undef, :$years = 0, :$months = 0, :$weeks = 0, :$days = 0, :$hours = 0, :$minutes = 0, :$seconds = 0) {
+    croak "Not a Time::C object: $start" unless ref $start and $start->isa('Time::C');
+    croak "Not a Time::C object: end => $end" if defined $end and not (ref $end and $end->isa('Time::C'));
+
+    bless {
+        start   => $start,
+        current => $start->clone(),
+        end     => $end,
+        years   => $years,
+        months  => $months,
+        weeks   => $weeks,
+        days    => $days,
+        hours   => $hours,
+        minutes => $minutes,
+        seconds => $seconds,
+    }, $c;
+}
+
 1;
