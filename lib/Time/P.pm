8d7c4fb6150232eacb599ccdf85e459d2555e8ba
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2016-10-29 02:04:52 +0200

    Time::P: rewrite to use a regex for parsing

--- lib/Time/P.pm
+++ lib/Time/P.pm
@@ -18,321 +18,162 @@ my %parser; %parser = (
 #
 #    %A - national representation of the full weekday name (eg Monday)
 
-    '%A' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
+    '%A' => fun () {
         my $re = _list2re(@weekdays);
-        if ($str =~ /\G($re)/) { return $struct->{'%A'} = $1; }
-        return undef;
+        return "(?<A>$re)";
     },
 
 #    %a - national representation of the abbreviated weekday name (eg Mon)
 
-    '%a' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
+    '%a' => fun () {
         my $re = _list2re(@weekdays_abbr);
-        if ($str =~ /\G($re)/) { return $struct->{'%a'} = $1; }
-        return undef;
+        return "(?<a>$re)";
     },
 
 #    %B - national representation of the full month name (eg January)
 
-    '%B' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
+    '%B' => fun () {
         my $re = _list2re(@months);
-        if ($str =~ /\G($re)/) { return $struct->{'%B'} = $1; }
-        return undef;
+        return "(?<B>$re)";
     },
 
 #    %b - national representation of the abbreviated month name (eg Jan)
 
-    '%b' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
+    '%b' => fun () {
         my $re = _list2re(@months_abbr);
-        if ($str =~ /\G($re)/) { return $struct->{'%b'} = $1; }
-        return undef;
+        return "(?<b>$re)";
     },
 
 #    %C - 2 digit century (eg 20)
 
-    '%C' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%C'} = $1; }
-        return undef;
-    },
+    '%C' => fun () { "(?<C>[0-9][0-9])"; },
 
 #    %D - equivalent to %m/%d/%y (eg 01/31/16)
 
-    '%D' => fun ($str, $pos, $struct) {
-        my $m = $parser{'%m'}->($str, $pos, $struct) // return undef;
-        $pos += length($m);
-        substr($str, $pos, 1) eq '/' or return undef;
-        my $d = $parser{'%d'}->($str, ++$pos, $struct) // return undef;
-        $pos += length($d);
-        substr($str, $pos, 1) eq '/' or return undef;
-        my $y = $parser{'%y'}->($str, ++$pos, $struct) // return undef;
-        return $struct->{'%D'} = "$m/$d/$y";
-    },
+    '%D' => fun () { sprintf "(?<D>%s/%s/%s)", $parser{'%m'}->(), $parser{'%d'}->(), $parser{'%y'}->(); },
 
 #    %d - 2 digit day of month (eg 30)
 
-    '%d' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%d'} = $1; }
-        return undef;
-    },
+    '%d' => fun () { "(?<d>[0-9][0-9])"; },
 
 #    %e - 1/2 digit day of month (eg 9)
 
-    '%e' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9]?)/) { return $struct->{'%e'} = $1; }
-        return undef;
-    },
+    '%e' => fun () { "(?<e>[0-9][0-9]?)"; },
 
 #    %F - equivalent to %Y-%m-%d (eg 2016-01-31)
 
-    '%F' => fun ($str, $pos, $struct) {
-        my $Y = $parser{'%Y'}->($str, $pos, $struct) // return undef;
-        $pos += length($Y);
-        substr($str, $pos, 1) eq '-' or return undef;
-        my $m = $parser{'%m'}->($str, ++$pos, $struct) // return undef;
-        $pos += length($m);
-        substr($str, $pos, 1) eq '-' or return undef;
-        my $d = $parser{'%d'}->($str, ++$pos, $struct) // return undef;
-        return $struct->{'%F'} = "$Y-$m-$d";
-    },
+    '%F' => fun () { sprintf "(?<F>%s-%s-%s)", $parser{'%Y'}->(), $parser{'%m'}->(), $parser{'%d'}->(); },
 
 #    %H - 2 digit hour in 24-hour time (eg 23)
 
-    '%H' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%H'} = $1; }
-        return undef;
-    },
+    '%H' => fun () { "(?<H>[0-9][0-9])"; },
 
 #    %h - equivalent to %b (eg Jan)
 
-    '%h' => fun ($str, $pos, $struct) {
-        return $struct->{'%h'} = $parser{'%b'}->($str, $pos, $struct);
-    },
+    '%h' => fun () { $parser{'%b'}->() },
 
 #    %I - 2 digit hour in 12-hour time (eg 11)
 
-    '%I' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%I'} = $1; }
-        return undef;
-    },
+    '%I' => fun () { "(?<I>[0-9][0-9])"; },
 
 #    %j - 3 digit day of the year (eg 001)
 
-    '%j' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9][0-9])/) { return $struct->{'%j'} = $1; }
-        return undef;
-    },
+    '%j' => fun () { "(?<j>[0-9][0-9][0-9])"; },
 
 #    %k - 1/2 digit hour in 24-hour time (eg 9)
 
-    '%k' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9]?)/) { return $struct->{'%k'} = $1; }
-        return undef;
-    },
+    '%k' => fun () { "(?<k>[0-9][0-9]?)"; },
 
 #    %l - 1/2 digit hour in 12-hour time (eg 9)
 
-    '%l' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9]?)/) { return $struct->{'%l'} = $1; }
-        return undef;
-    },
+    '%l' => fun () { "(?<l>[0-9][0-9]?)"; },
 
 #    %M - 2 digit minute (eg 45)
 
-    '%M' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%M'} = $1; }
-        return undef;
-    },
+    '%M' => fun () { "(?<M>[0-9][0-9])"; },
 
 #    %m - 2 digit month (eg 12)
 
-    '%m' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%m'} = $1; }
-        return undef;
-    },
+    '%m' => fun () { "(?<m>[0-9][0-9])"; },
 
-#    %n - newline
+#    %n - newline - arbitrary whitespace
 
-    '%n' => fun ($str, $pos, $struct) {
-        if (substr($str, $pos, 1) eq "\n") { return "\n"; }
-        return undef;
-    },
+    '%n' => fun () { "\\s+"; },
 
 #    %p - national representation of a.m./p.m.
 
-    '%p' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
+    '%p' => fun () {
         my $re = _list2re(@am_pm);
-        if ($str =~ /\G($re)/) { return $struct->{'%p'} = $1; }
-        return undef;
+        return "(?<p>$re)";
     },
 
 #    %R - equivalent to %H:%M (eg 22:05)
 
-    '%R' => fun ($str, $pos, $struct) {
-        my $H = $parser{'%H'}->($str, $pos, $struct) // return undef;
-        $pos += length($H);
-        substr($str, $pos, 1) eq ':' or return undef;
-        my $M = $parser{'%M'}->($str, ++$pos, $struct) // return undef;
-        return $struct->{'%R'} = "$H:$M";
-    },
+    '%R' => fun () { sprintf "(?<R>%s:%s)", $parser{'%H'}->(), $parser{'%M'}->(); },
 
 #    %r - equivalent to %I:%M:%S %p (eg 10:05:00 p.m.)
 
-    '%r' => fun ($str, $pos, $struct) {
-        my $I = $parser{'%I'}->($str, $pos, $struct) // return undef;
-        $pos += length($I);
-        substr($str, $pos, 1) eq ':' or return undef;
-        my $M = $parser{'%M'}->($str, ++$pos, $struct) // return undef;
-        $pos += length($M);
-        substr($str, $pos, 1) eq ':' or return undef;
-        my $S = $parser{'%S'}->($str, ++$pos, $struct) // return undef;
-        $pos += length($S);
-        substr($str, $pos, 1) eq ' ' or return undef;
-        my $p = $parser{'%p'}->($str, ++$pos, $struct) // return undef;
-        return $struct->{'%r'} = "$I:$M:$S $p";
-    },
+    '%r' => fun () { sprintf "(?<r>%s:%s:%s%s%s)", $parser{'%I'}->(), $parser{'%M'}->(), $parser{'%S'}->(), "\\s+", $parser{'%p'}->(); },
 
 #    %S - 2 digit second
 
-    '%S' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%S'} = $1; }
-        return undef;
-    },
+    '%S' => fun () { "(?<S>[0-9][0-9])"; },
 
 #    %s - 1/2/3/4/5/... digit seconds since epoch (eg 1477629064)
 
-    '%s' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9]+)/) { return $struct->{'%s'} = $1; }
-        return undef;
-    },
+    '%s' => fun () { "(?<s>[0-9]+)"; },
 
 #    %T - equivalent to %H:%M:%S
 
-    '%T' => fun ($str, $pos, $struct) {
-        my $H = $parser{'%H'}->($str, $pos, $struct) // return undef;
-        $pos += length($H);
-        substr($str, $pos, 1) eq ':' or return undef;
-        my $M = $parser{'%M'}->($str, ++$pos, $struct) // return undef;
-        $pos += length($M);
-        substr($str, $pos, 1) eq ':' or return undef;
-        my $S = $parser{'%S'}->($str, ++$pos, $struct) // return undef;
-        return $struct->{'%T'} = "$H:$M:$S";
-    },
+    '%T' => fun () { sprintf "(?<T>%s:%s:%s)", $parser{'%H'}->(), $parser{'%M'}->(), $parser{'%S'}->(); },
 
-#    %t - tab
+#    %t - tab - arbitrary whitespace
 
-    '%t' => fun ($str, $pos, $struct) {
-        if (substr($str, $pos, 1) eq "\t") { return "\t"; }
-        return undef;
-    },
+    '%t' => fun () { "\\s+"; },
 
 #    %U - 2 digit week number of the year Sunday-based week (eg 00)
 
-    '%U' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%U'} = $1; }
-        return undef;
-    },
+    '%U' => fun () { "(?<U>[0-9][0-9])"; },
 
 #    %u - 1 digit weekday Monday-based week (eg 1)
 
-    '%u' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9])/) { return $struct->{'%u'} = $1; }
-        return undef;
-    },
+    '%u' => fun () { "(?<u>[0-9])"; },
 
 #    %V - 2 digit week number of the year Monday-based week (eg 01)
 
-    '%V' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%V'} = $1; }
-        return undef;
-    },
+    '%V' => fun () { "(?<V>[0-9][0-9])"; },
 
 #    %v - equivalent to %e-%b-%Y (eg 9-Jan-2016)
 
-    '%v' => fun ($str, $pos, $struct) {
-        my $e = $parser{'%e'}->($str, $pos, $struct) // return undef;
-        $pos += length($e);
-        substr($str, $pos, 1) eq '-' or return undef;
-        my $b = $parser{'%b'}->($str, ++$pos, $struct) // return undef;
-        $pos += length($b);
-        substr($str, $pos, 1) eq '-' or return undef;
-        my $Y = $parser{'%Y'}->($str, ++$pos, $struct) // return undef;
-        return $struct->{'%v'} = "$e-$b-$Y";
-    },
+    '%v' => fun () { sprintf "(?<v>%s-%s-%s)", $parser{'%e'}->(), $parser{'%b'}->(), $parser{'%Y'}->(); },
 
 #    %W - 2 digit week number of the year Monday-based week (eg 00)
 
-    '%W' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%W'} = $1; }
-        return undef;
-    },
+    '%W' => fun () { "(?<W>[0-9][0-9])"; },
 
 #    %w - 1 digit weekday Sunday-based week (eg 0)
 
-    '%w' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9])/) { return $struct->{'%w'} = $1; }
-        return undef;
-    },
+    '%w' => fun () { "(?<w>[0-9])"; },
 
 #    %Y - 1/2/3/4 digit year including century (eg 2016)
 
-    '%Y' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9]{1,4})/) { return $struct->{'%Y'} = $1; }
-        return undef;
-    },
+    '%Y' => fun () { "(?<Y>[0-9]{1,4})"; },
 
 #    %y - 2 digit year without century (eg 99)
 
-    '%y' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([0-9][0-9])/) { return $struct->{'%y'} = $1; }
-        return undef;
-    },
+    '%y' => fun () { "(?<y>[0-9][0-9])"; },
 
 #    %Z - time zone name (eg CET)
 
-    '%Z' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G(\S+)/) { return $struct->{'%Z'} = $1; }
-        return undef;
-    },
+    '%Z' => fun () { "(?<Z>\S+)"; },
 
 #    %z - time zone offset from UTC (eg +0100)
 
-    '%z' => fun ($str, $pos, $struct) {
-        pos($str) = $pos;
-        if ($str =~ /\G([-+][0-9][0-9](?::?[0-9][0-9])?)/) { return $struct->{'%z'} = $1; }
-        return undef;
-    },
+    '%z' => fun () { "(?<z>[-+][0-9][0-9](?::?[0-9][0-9])?)"; },
 
 #    %% - percent sign
 
-    '%%' => fun ($str, $pos, $struct) {
-        if (substr($str, $pos, 1) eq '%') { return '%'; }
-        return undef;
-    },
+    '%%' => fun () { "%"; },
 
 );
 
@@ -342,27 +183,15 @@ sub strptime {
     my $s_pos = my $f_pos = 0;
     my $struct = {};
 
-    while ($s_pos < length $str and $f_pos < length $fmt) {
-        my $tok = _get_tok($fmt, $f_pos);
+    my $re = _compile_fmt($fmt);
 
-        my $ret;
-        if (exists $parser{$tok}) {
-            $ret = $parser{$tok}->($str, $s_pos, $struct);
-            croak sprintf "Could not match %s using %s. Match failed at position %d.", $str, $fmt, $s_pos
-              if not defined $ret;
-        } else {
-            $ret = substr($str, $s_pos, length($tok));
-            croak sprintf "Could not match %s using %s. Match failed at position %d.", $str, $fmt, $s_pos
-              if $ret ne $tok;
-        }
-        $s_pos += length($ret)
+    warn "fmt re: $re\n";
 
+    if ($str =~ $re) {
+        %$struct = %+;
+    } else {
+        croak sprintf "Could not match '%s' using '%s'.", $str, $fmt;
     }
-
-    croak sprintf "Could not match %s using %s. Trailing characters at position %d.", $str, $fmt, $s_pos
-      if $s_pos < length $str;
-    croak sprintf "Could not match %s using %s. Unexpected end of string at position %d.", $str, $fmt, $s_pos
-      if $f_pos < length $fmt;
 
     my ($time, $err) = _mktime($struct);
     croak sprintf "Could not match %s using %s. Invalid time specification: %s.", $str, $fmt, $err
@@ -371,8 +200,27 @@ sub strptime {
     return $time;
 }
 
+fun _compile_fmt ($fmt) {
+    my $re = "";
+
+    my $pos = 0;
+
+    # _get_tok will increment $pos for us
+    while (defined(my $tok = _get_tok($fmt, $pos))) {
+        if (exists $parser{$tok}) {
+            $re .= $parser{$tok}->();
+        } else {
+            $re .= quotemeta $tok;
+        }
+    }
+
+    return qr/$re/;
+}
+
 sub _get_tok {
     my ($fmt, $pos) = @_;
+
+    return undef if $pos >= length $fmt;
 
     my $tok_len = substr($fmt, $pos, 1) eq '%' ? 2 : 1;
 
@@ -389,51 +237,51 @@ fun _mktime ($struct) {
     require Time::C;
 
     # First, if we know the epoch, great
-    my $epoch = $struct->{'%s'};
+    my $epoch = $struct->{'s'};
 
     # Then set up as many date bits we know about
     #  year + day of year
     #  year + month + day of month
     #  year + week + day of week
 
-    my $year = $struct->{'%Y'};
+    my $year = $struct->{'Y'};
     if (not defined $year) {
-        if (defined $struct->{'%C'}) {
-            $year = $struct->{'%C'} * 100;
-            $year += $struct->{'%y'} if defined $struct->{'%y'};
-        } elsif (defined $struct->{'%y'}) {
-            $year = $struct->{'%y'} + 1900;
+        if (defined $struct->{'C'}) {
+            $year = $struct->{'C'} * 100;
+            $year += $struct->{'y'} if defined $struct->{'y'};
+        } elsif (defined $struct->{'y'}) {
+            $year = $struct->{'y'} + 1900;
             if ($year < (Time::C->now_utc()->year - 50)) { $year += 100; }
         }
     }
 
-    my $yday = $struct->{'%j'};
+    my $yday = $struct->{'j'};
 
-    my $month = $struct->{'%m'};
+    my $month = $struct->{'m'};
     if (not defined $month) {
-        if (defined $struct->{'%B'}) {
-            $month = _get_index($struct->{'%B'}, @months) + 1;
-        } elsif (defined $struct->{'%b'}) {
-            $month = _get_index($struct->{'%b'}, @months_abbr) + 1;
+        if (defined $struct->{'B'}) {
+            $month = _get_index($struct->{'B'}, @months) + 1;
+        } elsif (defined $struct->{'b'}) {
+            $month = _get_index($struct->{'b'}, @months_abbr) + 1;
         } 
     }
 
-    my $mday = $struct->{'%d'};
-    if (not defined $mday) { $mday = $struct->{'%e'}; }
+    my $mday = $struct->{'d'};
+    if (not defined $mday) { $mday = $struct->{'e'}; }
 
-    my $s_week = $struct->{'%U'};
-    my $m_week = $struct->{'%W'};
+    my $s_week = $struct->{'U'};
+    my $m_week = $struct->{'W'};
 
-    my $wday = $struct->{'%u'};
+    my $wday = $struct->{'u'};
     if (not defined $wday) {
-        $wday = $struct->{'%w'};
+        $wday = $struct->{'w'};
         $wday = 7 if defined $wday and $wday = 0;
     }
     if (not defined $wday) {
-        if (defined $struct->{'%A'}) {
-            $wday = _get_index($struct->{'%A'}, @weekdays) + 1;
-        } elsif (defined $struct->{'%a'}) {
-            $wday = _get_index($struct->{'%a'}, @weekdays_abbr) + 1;
+        if (defined $struct->{'A'}) {
+            $wday = _get_index($struct->{'A'}, @weekdays) + 1;
+        } elsif (defined $struct->{'a'}) {
+            $wday = _get_index($struct->{'a'}, @weekdays_abbr) + 1;
         }
     }
 
@@ -443,24 +291,24 @@ fun _mktime ($struct) {
 
     # Next try to set up time bits -- these are pretty easy in comparison
 
-    my $hour = $struct->{'%H'};
-    if (not defined $hour) { $hour = $struct->{'%k'}; }
+    my $hour = $struct->{'H'};
+    if (not defined $hour) { $hour = $struct->{'k'}; }
     if (not defined $hour) {
-        $hour = $struct->{'%I'} // $struct->{'%l'};
-        if (defined $hour and defined $struct->{'%p'}) {
-            $hour = _get_index($struct->{'%p'}, @am_pm) ? $hour + 12 : $hour;
+        $hour = $struct->{'I'} // $struct->{'l'};
+        if (defined $hour and defined $struct->{'p'}) {
+            $hour = _get_index($struct->{'p'}, @am_pm) ? $hour + 12 : $hour;
         }
     }
 
-    my $min = $struct->{'%M'};
+    my $min = $struct->{'M'};
 
-    my $sec = $struct->{'%S'};
+    my $sec = $struct->{'S'};
 
     # And last see if we have some timezone or at least offset info
 
-    my $tz = $struct->{'%Z'};
+    my $tz = $struct->{'Z'};
 
-    my $offset = $struct->{'%z'};
+    my $offset = $struct->{'z'};
 
     my $offset_n = defined $offset ? _offset_to_minutes($offset) : undef;
 
